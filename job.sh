65a7493b2195f13c501306c52acbfffbd069bd6e
chenxiaolong <chenxiaolong@master>
2013-07-30 16:15:57 -0400

    Make Jenkins script suitable for my setup

--- job.sh
+++ job.sh
@@ -1,16 +1,6 @@
-if [ -z "$HOME" ]
-then
-  echo HOME not in environment, guessing...
-  export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
-fi
+cd ${WORKSPACE}
 
-cd $WORKSPACE
-mkdir -p ../android
-cd ../android
-export WORKSPACE=$PWD
-
-if [ ! -d hudson ]
-then
+if [ ! -d hudson ]; then
   git clone git://github.com/chenxiaolong/hudson.git
 fi
 
