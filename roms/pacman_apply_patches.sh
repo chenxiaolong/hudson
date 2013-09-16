842e5146f4f2a76684129a87f8396be21fc3ebb8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-09-15 21:38:49 -0400

    Update app moving patches for Pacman

--- roms/pacman_apply_patches.sh
+++ roms/pacman_apply_patches.sh
@@ -29,7 +29,8 @@ reset_to_branch() {
 # system/vold/
 reset_to_branch system/vold/ cm/cm-10.2
 apply_patch_in system/vold/ \
-  move-app-to-sd/0001-vold-Allow-ASEC-containers-on-external-SD-when-inter.patch
+  move-app-to-sd/0001-vold-Allow-ASEC-containers-on-external-SD-when-inter.patch \
+  move-app-to-sd/0001-Vold-Do-not-mount-ASEC-containers-if-moving-apps-to-.patch
 
 # device/samsung/jf-common/
 reset_to_branch device/samsung/jf-common/ github/cm-10.2
@@ -40,20 +41,28 @@ apply_patch_in device/samsung/jf-common/ \
 reset_to_branch packages/apps/Settings/ pac/cm-10.2
 apply_patch_in packages/apps/Settings/ \
   move-app-to-sd/0001-Enable-moving-applications-to-the-external-SD-card.patch \
+  move-app-to-sd/0001-Add-app-moving-setting-to-development-options.patch \
   high-touch-sensitivity/0001-Add-preferences-for-high-touch-sensitivity.patch \
   high-touch-sensitivity/0001-Auto-copied-translations-for-high-touch-sensitivity.patch
 
 # frameworks/base/
 reset_to_branch frameworks/base/ pac/cm-10.2
 apply_patch_in frameworks/base/ \
-  move-app-to-sd/0001-Framework-changes-for-moving-applications-to-externa.patch
+  move-app-to-sd/0001-Framework-changes-for-moving-applications-to-externa.patch \
+  move-app-to-sd/0001-Framework-Check-of-moving-apps-to-SD-is-disabled.patch
 
 # frameworks/opt/hardware/
 reset_to_branch frameworks/opt/hardware/ cm/cm-10.2
 apply_patch_in frameworks/opt/hardware/ \
-  high-touch-sensitivity/0001-Hardware-Add-high-touch-sensitivity-support.patch
+  high-touch-sensitivity/0001-Hardware-Add-high-touch-sensitivity-support.patch \
+  move-app-to-sd/0001-Add-class-for-enabling-and-disabling-moving-apps-to-.patch
 
 # hardware/samsung/
 reset_to_branch hardware/samsung/ github/cm-10.2
 apply_patch_in hardware/samsung/ \
   high-touch-sensitivity/0001-Samsung-add-support-for-high-touch-sensitivity.patch
+
+# frameworks/native/
+reset_to_branch frameworks/native/ pac/cm-10.2
+apply_patch_in frameworks/native/ \
+  move-app-to-sd/0001-Calculate-application-sizes-correctly.patch
