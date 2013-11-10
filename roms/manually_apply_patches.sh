40ffc9cc515a9302d2f35962a7d74c92b7b51017
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-10 17:57:49 -0500

    Add script for manually applying patches

---
+++ roms/manually_apply_patches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! [ "${#}" -eq 2 ]; then
+  echo "Usage: ${0} [workspace directory] [branch]"
+fi
+
+export REPO_BRANCH="${2}"
+export WORKSPACE="${1}"
+
+source ${WORKSPACE}/hudson/common.sh
+source ${WORKSPACE}/hudson/roms/${REPO_BRANCH}.sh
+
+reset_dirs_${REPO_BRANCH}
+apply_patches_${REPO_BRANCH}
