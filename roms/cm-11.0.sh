8d0c88a9d9075bd77c82e5b73508c5f4b2f27843
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-20 17:56:46 -0500

    [KitKat] Remove my screwed up bcmdhd patches

--- roms/cm-11.0.sh
+++ roms/cm-11.0.sh
@@ -5,7 +5,6 @@ reset_dirs_cm-11.0() {
     'art/'
     'device/samsung/qcom-common/'
     'packages/providers/ContactsProvider/'
-    'frameworks/native/'
     'packages/apps/Dialer/'
     'packages/services/Telephony/'
     'external/clang/'
@@ -30,6 +29,7 @@ reset_dirs_cm-11.0() {
     'system/vold/'
     'packages/apps/Settings/'
     'frameworks/base/'
+    'frameworks/native/'
   )
 
   for i in ${RESET_DIRS[@]} ${RESET_DIRS_OLD[@]}; do
@@ -41,12 +41,8 @@ reset_dirs_cm-11.0() {
 
 apply_patches_cm-11.0() {
   PATCHES=${WORKSPACE}/hudson/roms/${REPO_BRANCH}
-  HIGHTOUCHSENSITIVITY=${PATCHES}/high-touch-sensitivity
   FACEBOOKSYNC=${PATCHES}/facebook-sync
-  MOVEAPPTOSD=${PATCHES}/move-app-to-sd
-  SENSORS=${PATCHES}/sensors
   DUALBOOT=${PATCHES}/dual-boot
-  BCMDHD=${PATCHES}/bcmdhd
 
   pushd art/
   apply_patch_file_git ${PATCHES}/0001-Run-generate-operator-out.py-with-Python-2.patch
@@ -54,33 +50,6 @@ apply_patches_cm-11.0() {
 
   pushd packages/providers/ContactsProvider/
   apply_patch_file_git ${FACEBOOKSYNC}/0001-ContactsProvider-Hack-to-enable-Facebook-contacts-sy.patch
-  popd
-
-  pushd frameworks/native/
-  # Thanks to Team Guppy for finding the commits to revert!
-  # https://github.com/TEAM-Gummy/platform_frameworks_native/commit/4980b82179fb742a830e3fc68781e35bb4a3ee81
-  # https://github.com/TEAM-Gummy/platform_frameworks_native/commit/9f73d1e2e1ca4f59abe5a5ef2b607b8996f67bd2
-  # https://github.com/TEAM-Gummy/platform_frameworks_native/commit/59fcdbef1e75bef84d22f8353544973807bb785a
-  apply_patch_file_git ${SENSORS}/0001-revert-to-4.3-sensors-for-testing.patch
-  apply_patch_file_git ${SENSORS}/0002-complete-the-sensor-swapout.patch
-  apply_patch_file_git ${SENSORS}/0003-Make-Flattenable-not-virtual.patch
-  popd
-
-  pushd kernel/samsung/jf/
-  # Patches from https://github.com/franciscofranco/hammerhead.git
-  # Thanks to Entropy512/Andrew Dodd and Francisco Franco for finding them!
-  # https://plus.google.com/109625418534467664286/posts/7X4sUWPKeGX
-
-  # 837961187ec3ecf82a11b89d7cf8fb267d3ed9ce
-  #apply_patch_file_git ${BCMDHD}/0001-net-wireless-bcmdhd-fixed-power-consumption-issue-of.patch
-  # 24bf61016206ba6fb0edb2b4593f8197fd89137e
-  #apply_patch_file_git ${BCMDHD}/0001-net-wireless-bcmdhd-Fixed-a-problem-of-buganizer-iss.patch
-  # 5dd724eb19f768e641041a0b897c5e8464077949
-  #apply_patch_file_git ${BCMDHD}/0001-net-wireless-bcmdhd-reduced-the-wakelock-time-of-RX-.patch
-  # c8092f60419fcec95cc5c4b458de9d6be2fc60fc
-  #apply_patch_file_git ${BCMDHD}/0001-net-wireless-bcmdhd-Change-DTIM-skip-policy-in-suspe.patch
-  # 6076d5a4d5724b6f941a0ca86d62708786cdab84
-  #apply_patch_file_git ${BCMDHD}/0001-drivers-bcmdhd-filter-only-unicast-packets-during-su.patch
   popd
 
   GERRIT_URL="http://review.cyanogenmod.org" \
