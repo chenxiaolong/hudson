ebd6352caf66d176d4129097c077adfe6b2e16a9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-26 04:27:55 -0500

    [KitKat] Stupid time_daemon

--- roms/cm-11.0.sh
+++ roms/cm-11.0.sh
@@ -69,7 +69,7 @@ apply_patches_cm-11.0() {
     'http://review.cyanogenmod.org/#/c/53635/' `# jf-common: Fix GPS`                                                \
     'http://review.cyanogenmod.org/#/c/53969/' `# jf: fix fstab`                                                     \
     `# system/core` \
-    'http://review.cyanogenmod.org/#/c/53310/' `# Add support for QCs time_daemon`                                   \
+    `# 'http://review.cyanogenmod.org/#/c/53310/'` `# Add support for QCs time_daemon`                                   \
     `# packages/apps/Dialer` \
     'http://review.cyanogenmod.org/#/c/53302/' `# Dialer: Update Icons to KitKat`                                    \
     `# packages/services/Telephony` \
