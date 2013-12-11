5167ecfc9da74a0263be94de7e275c434b5f1a4d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-11 10:16:16 -0500

    Clean every 18 hours to ensure nightly builds have a newly generated build.prop

--- common.sh
+++ common.sh
@@ -104,10 +104,7 @@ common_prebuild() {
   TIME_SINCE_LAST_CLEAN=$(expr $(date +%s) - ${LAST_CLEAN})
   # convert this to hours
   TIME_SINCE_LAST_CLEAN=$(expr ${TIME_SINCE_LAST_CLEAN} / 60 / 60)
-###
-#  TIME_SINCE_LAST_CLEAN=1
-###
-  if [ "${TIME_SINCE_LAST_CLEAN}" -gt "24" -o "${CLEAN}" = "true" ]; then
+  if [ "${TIME_SINCE_LAST_CLEAN}" -gt "18" -o "${CLEAN}" = "true" ]; then
     echo "Cleaning!"
     touch .clean
     set -e
