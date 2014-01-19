7db4be4b57ded24bce99e4badc4c90ecfaea8b18
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-01-18 22:04:43 -0500

    cm-11.0: Some fixes

--- roms/cm-11.0.sh
+++ roms/cm-11.0.sh
@@ -1,14 +1,12 @@
 reset_dirs_cm-11.0() {
   RESET_DIRS=(
     'packages/providers/ContactsProvider/'
-    'packages/apps/Apollo/'
     'packages/apps/Dialer/'
-    'device/samsung/jf-common/'
+    'packages/apps/InCallUI/'
   )
 
   # Directories that should be reset for one more build
   RESET_DIRS_OLD=(
-    'external/bluetooth/bluedroid/'
   )
 
   for i in ${RESET_DIRS[@]} ${RESET_DIRS_OLD[@]}; do
@@ -35,19 +33,13 @@ apply_patches_cm-11.0() {
   apply_patch_file_git ${GOOGLEDIALER}/0001-Open-source-Google-Dialer.patch
   apply_patch_file_git ${GOOGLEDIALER}/0001-Auto-merge-Google-Dialer-translations.patch
   apply_patch_file_git ${GOOGLEDIALER}/0001-Re-add-LoaderCallbacks-to-CyanogenMod-dialer.patch
+  popd
+
+  pushd packages/apps/InCallUI/
   apply_patch_file_git ${GOOGLEDIALER}/0001-InCallUI-Google-Phone-Number-Service.patch
   popd
 
-  python3 ${WORKSPACE}/hudson/gerrit_changes.py \
-    `# device/samsung/jf-common`                                      \
-    'http://review.cyanogenmod.org/#/c/53635/' `# jf-common: Fix GPS` \
-    `# packages/apps/Apollo`                                          \
-    'http://review.cyanogenmod.org/#/c/54722/'                        \
-
-#    `# Camera stuff` \
-#    I26898b82f6c9ab81e6f1681805de229e4ac2f308 \
-#    I56739157380f596c9f3bbbe7aecd8f532d619c72 \
-#    Ia0f5716d5e6815d249040b08313482a103a36863 \
-#    I216502fe032a89f69e1aea11bc50c51634d40991 \
-#    Ib36bd21c9a76b45bced3eee2f25acc35b5d82b30
+#  python3 ${WORKSPACE}/hudson/gerrit_changes.py \
+#    `# device/samsung/jf-common`                                      \
+#    'http://review.cyanogenmod.org/#/c/53635/' `# jf-common: Fix GPS`
 }
