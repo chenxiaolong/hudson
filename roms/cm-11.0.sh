41007ed1b4dbd45ca44d4dbd84243566cfe96eb0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-01-30 22:05:25 -0500

    Remove broken decompiled Google Dialer

--- roms/cm-11.0.sh
+++ roms/cm-11.0.sh
@@ -23,19 +23,15 @@ reset_dirs_cm-11.0() {
 apply_patches_cm-11.0() {
   PATCHES=${WORKSPACE}/hudson/roms/${REPO_BRANCH}
   FACEBOOKSYNC=${PATCHES}/facebook-sync
-  GOOGLEDIALER=${PATCHES}/google-dialer
 
   pushd packages/providers/ContactsProvider/
   apply_patch_file_git ${FACEBOOKSYNC}/0001-ContactsProvider-Hack-to-enable-Facebook-contacts-sy.patch
   popd
 
   pushd packages/apps/Dialer/
-  apply_patch_file_git ${GOOGLEDIALER}/0001-Open-source-Google-Dialer.patch
-  apply_patch_file_git ${GOOGLEDIALER}/0001-Auto-merge-Google-Dialer-translations.patch
   popd
 
   pushd packages/apps/InCallUI/
-  apply_patch_file_git ${GOOGLEDIALER}/0001-InCallUI-Google-Phone-Number-Service.patch
   popd
 
 #  python3 ${WORKSPACE}/hudson/gerrit_changes.py \
