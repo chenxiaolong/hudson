7f52939ded38994a796cc273daddcf206fd801cd
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-10 00:54:05 -0500

    Remove dialer patches: Merged via gerrit

--- roms/cm-11.0.sh
+++ roms/cm-11.0.sh
@@ -1,15 +1,15 @@
 reset_dirs_cm-11.0() {
   RESET_DIRS=(
     'packages/providers/ContactsProvider/'
-    'packages/apps/Dialer/'
-    'packages/apps/InCallUI/'
-    'packages/services/Telephony/'
-    'frameworks/base/'
   )
 
   # Directories that should be reset for one more build
   RESET_DIRS_OLD=(
     'packages/apps/ContactsCommon/'
+    'packages/apps/Dialer/'
+    'packages/apps/InCallUI/'
+    'packages/services/Telephony/'
+    'frameworks/base/'
   )
 
   for i in ${RESET_DIRS[@]} ${RESET_DIRS_OLD[@]}; do
@@ -26,27 +26,9 @@ reset_dirs_cm-11.0() {
 apply_patches_cm-11.0() {
   PATCHES=${WORKSPACE}/hudson/roms/${REPO_BRANCH}
   FACEBOOKSYNC=${PATCHES}/facebook-sync
-  DIALERLOOKUP=${PATCHES}/dialer-lookup
 
   pushd packages/providers/ContactsProvider/
   apply_patch_file_git ${FACEBOOKSYNC}/0001-ContactsProvider-Hack-to-enable-Facebook-contacts-sy.patch
-  popd
-
-  pushd packages/apps/Dialer/
-  apply_patch_file_git ${DIALERLOOKUP}/0001-Dialer-Add-support-for-forward-and-reverse-lookups.patch
-  popd
-
-  pushd packages/apps/InCallUI/
-  apply_patch_file_git ${DIALERLOOKUP}/0001-InCallUI-Add-phone-number-service.patch
-  popd
-
-  pushd packages/services/Telephony/
-  apply_patch_file_git ${DIALERLOOKUP}/0001-Telephony-Add-settings-for-forward-and-reverse-numbe.patch
-  popd
-
-  pushd frameworks/base/
-  apply_patch_file_git ${DIALERLOOKUP}/0001-AccountManagerService-Allow-com.android.dialer-to-ac.patch
-  apply_patch_file_git ${DIALERLOOKUP}/0001-Frameworks-Add-settings-keys-for-forward-and-reverse.patch
   popd
 
 #  python3 ${WORKSPACE}/hudson/gerrit_changes.py \
